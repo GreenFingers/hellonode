b478b3321d0df9a8f99876b8705edca551dd098b
Daniel Bruzual <daniel.bruzual@eficode.com>
2018-04-25 15:03:35 +0300

    Add full path to script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ pm2 describe backend > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -ne 0 ]; then
-  pm2 start main.js -i 0 --name backend
+  pm2 start /home/ec2-user/www/main.js -i 0 --name backend
 else
   pm2 reload backend
 fi;
