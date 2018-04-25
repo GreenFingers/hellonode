d99b5da08c90dce7b8549ae65282a9ac87fa9c47
Daniel Bruzual <daniel.bruzual@eficode.com>
2018-04-25 15:25:04 +0300

    V4

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello getintodevops.com v3\n");
+  response.end("Hello getintodevops.com v4 (with credentials)\n");
 });
 
 // listen on localhost:8000
